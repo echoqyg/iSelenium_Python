180bcfbe2ca5c76fd87e30b74115eddc6a61ba2f
xiaohui <296891196@qq.com>
2021-01-23 23:17:53 +0800

    第三天作业

--- day03/common/get_config.py
+++ day03/common/get_config.py
@@ -7,20 +7,24 @@ from day01.get_token import get_token
 
 
 class TestConfig():
+    # 设置地址
     _path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
     _path_2 = os.path.abspath(os.path.join(os.getcwd(), "../.."))
     _path_3 = os.path.join(_path_2, "day01/config.ini")
 
+    # 获取config.ini section, option的值
     def get_config(self):
         config = configparser.ConfigParser()
         config.read(self._path_3)
         return config.get('wechart', 'secret')
 
+    # 修改config.ini section, option的值
     def set_config(self):
         config = configparser.ConfigParser()
         config.read(self._path_3)
-        config.set('test', 'age',"2")
+        config.set('test', 'age', "2")
         config.write(open(self._path_3, "w"))
+
 
 if __name__ == "__main__":
     # print(TestConfig().path_3)
--- day03/common/get_log.py
+++ day03/common/get_log.py
@@ -4,16 +4,24 @@ import logging
 
 
 class GetLog:
+    # 初始化生成器，设置格式
     def __init__(self):
+        # 创建生成器
         self.logger = logging.getLogger()
+        # 设置日志等级
         self.logger.setLevel(logging.DEBUG)
+        # 设置日志格式
         self.formatter = logging.Formatter("%(asctime)s|%(levelname)-6s|%(filename)s:%(lineno)-3s|%(message)s",
                                            "%Y-%m-%d-%H:%M")
-        self.stream_handle = logging.StreamHandler()
 
     def set_stream_handle(self):
+        # 创建流处理器
+        self.stream_handle = logging.StreamHandler()
+        # 设置流处理器等级
         self.stream_handle.setLevel(logging.DEBUG)
+        # 传入流处理器格式
         self.stream_handle.setFormatter(self.formatter)
+        # 把流处理器，传入生成器
         self.logger.addHandler(self.stream_handle)
 
 
